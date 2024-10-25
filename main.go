5f27640dd406d80645e4d75e66e9835f8b7cdca0
uachema <uachema@gmail.com>
2024-10-25 22:53:16 +0500

    basic server setup

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello World")
+	})
+	mux.HandleFunc("GET /{id}", func(w http.ResponseWriter, r *http.Request) {
+		v := r.PathValue("id")
+		fmt.Fprintln(w, "Hello World ", v)
+	})
+	log.Println("server started on 127.0.0.1:3000")
+	log.Fatal(http.ListenAndServe("127.0.0.1:3000", mux))
 }
